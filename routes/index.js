3528b9c6dcbeed89aa103f802aec7f2e97f04334
pford68 <dev@philipford.com>
2026-02-16 10:31:20 -0500

    Using es6 imports.  Added eslint

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+import express from "express"
+const indexRouter = express.Router();
+
+/* GET home page. */
+indexRouter.get('/', (req, res, next) => {
+  res.render('index', { title: 'Express' });
+});
+
+export { indexRouter };
